e9928dab85cf104127a798b94c01f79874bb7411
miles <2604323152@qq.com>
2024-01-13 19:25:24 +0800

    first commit

---
+++ chapter1/main.js
@@ -0,0 +1,22 @@
+// console.log('app')
+
+// 屏幕显示 'app'
+
+//v1
+//文本内容是一个文本节点，
+// const root = document.querySelector("#root")
+// const text = document.createTextNode('app')
+// root.append(text)
+// root.append('app')
+
+//v2
+//用对象描述dom: 
+/* 
+{
+  type，
+  props：{
+    
+  }
+}
+*/
+
